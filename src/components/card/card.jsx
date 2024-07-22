299df69279b96bb907b289935b5c66b9609b2dae
subham jalan <subhamj13@gmail.com>
2024-07-23 00:12:57 +0530

    change

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,8 +3,10 @@ import "./Card.css";
 export default function Card ({name , img}){
     return(
         <div className="countryCard">
+            <div className="imgDiv">
                 <img className="flagImg" src={img} alt={name} />
-            <h3>{name}</h3>
+            </div>
+            <h2>{name}</h2>
         </div>
     )
 }
